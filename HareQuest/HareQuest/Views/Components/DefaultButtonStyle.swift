f145146e5cf3a97bf2bec0ed49eef5aab50b9305
Macsback <119421402+Macsback@users.noreply.github.com>
2026-04-21 14:44:01 +0100

    :lipstick: Finished Landing Page
    Any ideas, please comment them

--- HareQuest/HareQuest/Views/Components/DefaultButtonStyle.swift
+++ HareQuest/HareQuest/Views/Components/DefaultButtonStyle.swift
@@ -16,14 +16,17 @@ struct DefaultButtonStyle: ButtonStyle {
             configuration.label
             
         }
+        .shadow(color: .black.opacity(1), radius: 100, x: 0, y:20)
         .font(.system(.title3, design: .rounded, weight: .bold))
         .foregroundColor(.white)
+       
         .padding(.horizontal, 100)
         .padding(.vertical)
         .background {
-            RoundedRectangle(cornerRadius: 15)
+            RoundedRectangle(cornerRadius: 50)
                 .foregroundColor(color)
         }
+        
         .scaleEffect(configuration.isPressed ? 0.96 : 1.0)
     }
 }
--- HareQuest/HareQuest/Views/Landing/LandingView.swift
+++ HareQuest/HareQuest/Views/Landing/LandingView.swift
@@ -8,45 +8,74 @@
 import SwiftUI
 
 struct LandingView: View {
-	@StateObject private var controller = LandingController()
-	
-
-	var body: some View {
+    @StateObject private var controller = LandingController()
     
-           
-		NavigationStack { /// Parent navigation stack, this is passed to all child view (pretty much every view)
-            
-			VStack {
-                Text("Welcome to Hare Quest")
-                    .padding()
-                    .font(.title)
-                    .foregroundStyle(.accentTitle)
-                    .fontWeight(.bold)
-               
-				Button("Login") {
-					controller.openLoginView()
-				}
-                .buttonStyle(DefaultButtonStyle())
-                            
+    var body: some View {
+        NavigationStack { /// Parent navigation stack, this is passed to all child view (pretty much every view)
+            VStack {
                 
-				Button("Register") {
-					controller.openRegisterView()
-                }.foregroundStyle(.black)
-			}
-			.navigationDestination(item: $controller.currentRoute) { route in
-				switch route {
-					case .login:
-					LoginView()
-				case .register:
-					RegisterView()
-				}
-			}
-			
-		}.navigationBarBackButtonHidden(true)
-		
-	}
-}
-
+                Image("LandingImage")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(maxWidth: .infinity)
+                    .ignoresSafeArea(edges: .top)
+                    .padding(0)
+                    
+              
+                
+                ZStack {
+                    
+                    Rectangle()
+                        .fill(Color.white)
+                        .frame(height: 200)
+                        .shadow(color: Color.black.opacity(0.3),
+                                radius: 50, x:0, y:-100)
+                        .padding(0)
+                    VStack {
+                        
+                        Text("Welcome to Hare Quest")
+                            .padding()
+                            .font(.title)
+                            .foregroundStyle(.accentTitle)
+                            .fontWeight(.bold)
+                        
+                        
+                        Button("Login") {
+                            controller.openLoginView()
+                        }
+                        
+                        .buttonStyle(DefaultButtonStyle())
+                        
+                        
+                        
+                        HStack {
+                            Text("New to the burrow?")
+                            Button("Register") {
+                                controller.openRegisterView()
+                            }
+                            
+                            .foregroundStyle(.accentTitle)
+                        }
+                        .padding()
+                    }
+                }
+                
+              
+            }
+                
+                .navigationDestination(item: $controller.currentRoute) { route in
+                    switch route {
+                    case .login:
+                        LoginView()
+                    case .register:
+                        RegisterView()
+                    }
+                }
+            
+        }
+        .navigationBarBackButtonHidden(true)
+    }}
+    
 #Preview {
     LandingView()
 }
